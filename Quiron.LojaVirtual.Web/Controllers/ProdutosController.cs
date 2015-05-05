5f19188a91fab439bc7f47a5d9989311c764c3ef
maytonrocha <maytonrocha@gmail.com>
2015-05-05 17:19:50 -0300

    paginacao

--- Quiron.LojaVirtual.Web/Controllers/ProdutosController.cs
+++ Quiron.LojaVirtual.Web/Controllers/ProdutosController.cs
@@ -14,7 +14,7 @@ namespace Quiron.LojaVirtual.Web.Controllers
         public ActionResult Index()
         {
             _repositorio = new ProdutosRepositorio();
-            var Produtos = _repositorio.Produtos.Take(10);
+            var Produtos = _repositorio.Produtos;
 
             return View(Produtos);
         }
---
+++ Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
@@ -0,0 +1,26 @@
+﻿using Quiron.LojaVirtual.Dominio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    public class VitrineController : Controller
+    {
+        // GET: Produtos
+        private ProdutosRepositorio _repositorio;
+        private int ProdutosPorPagina = 8;
+        public ActionResult ListaProdutos(int pagina = 1)
+        {
+            _repositorio = new ProdutosRepositorio();
+            var Produtos = _repositorio.Produtos
+                .OrderBy(p => p.Descricao)
+                .Skip((pagina-1)*ProdutosPorPagina)
+                .Take(ProdutosPorPagina);
+
+            return View(Produtos);
+        }
+    }
+}

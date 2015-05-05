42c0bf50afe261da7166a098b8fff690789532c9
maytonrocha <maytonrocha@gmail.com>
2015-05-05 14:38:21 -0300

    context

--- Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
+++ Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Quiron.LojaVirtual.Dominio.Entidades
 {
-    public class Produtos
+    public class Produto
     {
         public int ProdutoId { get; set; }
         public string Nome { get; set; }
